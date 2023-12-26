519a83ed7f10dc0bf5b7805aa44ba0e0a371291f
Ahmed Ayman <ahmedayman4a77@gmail.com>
2023-12-26 18:58:08 +0200

    Add find by username function

--- server_side/mail/src/main/java/com/cse/mail/dal/repository/UserRepository.java
+++ server_side/mail/src/main/java/com/cse/mail/dal/repository/UserRepository.java
@@ -4,12 +4,17 @@ import com.cse.mail.dal.model.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("select u from User u where upper(u.username) = upper(:username)")
+    User findByUsernameIgnoreCase(@Param("username") @NonNull String username);
+
 
 
 }
