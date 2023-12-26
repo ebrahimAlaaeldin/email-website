fbb23bce63d23c2afa384bf38683ee4346db1315
Ahmed Ayman <ahmedayman4a77@gmail.com>
2023-12-26 22:38:52 +0200

    Rename test

--- server_side/mail/src/test/java/com/cse/mail/dal/repository/UserRepositoryIntegrationTest.java
+++ server_side/mail/src/test/java/com/cse/mail/dal/repository/UserRepositoryIntegrationTest.java
@@ -5,21 +5,16 @@ import com.cse.mail.dal.model.User;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
-public class UserRepositoryIntegerationTest {
+public class UserRepositoryIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
